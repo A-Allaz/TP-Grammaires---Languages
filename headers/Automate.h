ce820ee69a294ad9a86fac90f45ae92b0c01ddcd
ValAlphaM <alexis.gobin@insa-lyon.fr>
2025-02-25 00:00:25 +0100

    spacing

--- headers/Automate.h
+++ headers/Automate.h
@@ -8,9 +8,6 @@
 
 using namespace std;
 
-
-
-
 class Automate {
 public:
     // Constructor
@@ -23,17 +20,17 @@ public:
     void start();
 
     // decalage
-    void decalage(Symbole *s, State *e);
+    void decalage(Symbole* s, State* e);
 
     // reduction
-    void reduction(int n, Symbole *s, int ruleNumber);
+    void reduction(int n, Symbole* s, int ruleNumber);
     // n est la taille de la règle de réduction
     //
-    void transition_simple(Symbole *s, State *e);
-
+    void transition_simple(Symbole* s, State* e);
+ 
 private:
     // state stack 
-    stack<State *> stateStack;
+    stack<State*> stateStack;
     stack<Symbole*> symbolStack;
 
     // lexer
