850530d59b2bed229b58c0b5a59a49a93492c1de
ValAlphaM <alexis.gobin@insa-lyon.fr>
2025-02-25 00:01:06 +0100

    implementing main function

--- app/main.cpp
+++ app/main.cpp
@@ -8,11 +8,12 @@ int main(void) {
    string chaine("1+3");
    // string chaine("(1+34)*123");
 
-   // Lexer l(chaine);
+   Lexer lexer(chaine);
 
-   // Automate();
+   Automate automate(lexer);
+
+   automate.start(); // start the automate to verify the string
 
    return 0;
-
 }
 
